da153c86172ec5fc9d8051c8ef1883d28f0695ec
Atishay Jain <jainatishay208@gmail.com>
2026-02-01 22:43:36 +0530

    create own react library

--- 01basics/src/main.jsx
+++ 01basics/src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+
+
+const anotherElement = React.createElement(
+  'a',
+  {href:'https://google.com', target:'_blank'},
+  'CLick to visit google'
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  anotherElement
+  
 )
