de95b29f6da7a45bc6e7d89692552c26e68c05fa
Atishay Jain <jainatishay208@gmail.com>
2026-02-04 21:19:04 +0530

    currency convertor

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export function useCurrencyInfo(currency) {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        fetch(
+            `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+        )
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]));
+    }, [currency]);
+
+    return data;
+}
+
+export function useCurrencyFullForm() {
+    const [select, setSelect] = useState({});
+
+    useEffect(() => {
+        fetch(
+            `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json`
+        )
+            .then((res) => res.json())
+            .then((res) => setSelect(res));
+    }, []);
+
+    return select; 
+}
