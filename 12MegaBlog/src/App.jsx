931faad30913ae67a1ade6b1ee56c80470635505
Atishay Jain <jainatishay208@gmail.com>
2026-02-09 23:31:07 +0530

    Env and appwrite in react project

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+  return (
+    <>
+      <div>A BLOG APP WITH APPWRITE</div>  
+    </>
+  )
+}
+
+export default App
